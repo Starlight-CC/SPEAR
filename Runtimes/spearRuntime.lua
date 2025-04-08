8ac43981af0b6e360b8443be094e166a082ad8b3
Astronand <hunter.vinson@fwcsstudents.org>
2025-04-08 10:55:18 -0400

    e

--- Runtimes/spearRuntime.lua
+++ Runtimes/spearRuntime.lua
@@ -6,7 +6,8 @@ local SPEAR = {
     vars={},
     nums={"1","2","3","4","5","6","7","8","9","0"},
     chars={"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"},
-    operators={"true","false","==","~=",">","<","<=",">=","+","-","*","/","++","--"},
+    operators={"==","~=",">","<","<=",">=","+","-","*","/","++","--"},
+    static={"true","false","nil"},
     syntax={
         {name="print",mode={}},
         {name="do",mode={}},
@@ -39,8 +40,8 @@ local SPEAR = {
         {name="pub",mode=function() return SPEAR.copy("private") end}
     },
     Operations={
-        T=true,
-        F=false,
-        
+        T=function() return true end,
+        F=function() return false end,
+        N=function() return nil end
     }
 }
