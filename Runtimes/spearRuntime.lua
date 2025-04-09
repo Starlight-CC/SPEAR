891fe84df6a0fd2a3fd6acf1333b2541acef03a6
Astronand <hunter.vinson@fwcsstudents.org>
2025-04-09 10:29:58 -0400

    g

--- Runtimes/spearRuntime.lua
+++ Runtimes/spearRuntime.lua
@@ -10,5 +10,10 @@ local chars={"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q"
 local operators={"==","~=",">","<","<=",">=","+","-","*","/","++","--"}
 local static={"true","false","nil"}
 local syntax={
-    ["goto"] = {},
+    ["goto"] = {
+        call=function(...)
+            args = {...}
+            if type(args[1]) == 
+        end
+    },
 }
