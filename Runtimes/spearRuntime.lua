1011ad674871a6be5cbf0d0cbdd4c73e3f967903
Astronand <hunter.vinson@fwcsstudents.org>
2025-04-09 10:04:05 -0400

    e

--- Runtimes/spearRuntime.lua
+++ Runtimes/spearRuntime.lua
@@ -1,53 +1,14 @@
-config={
+local config={
     TOC=100
 }
-PC=1
-vars={}
-nums={"1","2","3","4","5","6","7","8","9","0"}
-chars={"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"}
-operators={"==","~=",">","<","<=",">=","+","-","*","/","++","--"}
-static={"true","false","nil"}
-syntax={
-    {name="print",mode={}},
-    {name="do",mode={}},
-    {name="end",mode={}},
-    {name="if",mode={}},
-    {name="while",mode={}},
-    {name="repeat",mode={}},
-    {name="goto",mode={}},
-    {name="gflag",mode={}},
-    {name="private",mode={
-        --Var types
-        {name="byte",mode={}},
-        {name="ubyte",mode={}},
-        {name="short",mode={}},
-        {name="ushort",mode={}},
-        {name="int",mode={}},
-        {name="uint",mode={}},
-        {name="long",mode={}},
-        {name="ulong",mode={}},
-        {name="sfloat",mode={}},
-        {name="float",mode={}},
-        {name="double",mode={}},
-        {name="quad",mode={}},
-        {name="string"mode={}},
-        {name="func",mode={}},
-        {name="table",mode={}},
-        {name="list",mode={}},
-        {name="bool",mode={}}
-    }},
-    {name="public",mode=function() return copy("private") end},
-    {name="goto",mode={}},
-    {name="prv",mode=function() return copy("private") end},
-    {name="pub",mode=function() return copy("private") end}
-}
-Operations={
-    T=function() return true end,
-    F=function() return false end,
-    N=function() return nil end
-}
---0 is name
---tbl is a numbered arg
---1 unnumbered arg
-modes={
+local PC=1
+local LC=1
+local vars={}
+local gotoFlags={}
+local nums={"1","2","3","4","5","6","7","8","9","0"}
+local chars={"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"}
+local operators={"==","~=",">","<","<=",">=","+","-","*","/","++","--"}
+local static={"true","false","nil"}
+local syntax={
+    ["goto"] = {},
 }
