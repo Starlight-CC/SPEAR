da89d2711825c18bc10e059b1a4717ddcd20de5f
Astronand <hunter.vinson@fwcsstudents.org>
2025-04-10 08:35:38 -0400

    e

--- Demos/gotoDemo.sp
+++ Demos/gotoDemo.sp
@@ -1,12 +1,12 @@
 print("goto Demo") // Prints string to Standard output
-goto "hi" // Jump to a flag.
-prv int A = 11 // Declare "A" as a private integer
+gflag "Hi" // Declares flag
+goto 10 // Jump to line 10
 A = A-- // "A--" Returns "A-1"
 if A ~= 0 do // If "A" is not equal to "0" returns True
   print("A is "..tostr(A)) // Converts "A" to a string then joins it to "A is " to make "A is #"
   goto 4 // Jump to line 4
 end // End "if" statement
-goto 11 // Jump to line 11
-gflag "Hi" // Declares flag
-goto 3 // Jump to 3
+goto 12 // Jump to line 12
+prv int A = 11 // Declare "A" as a private integer
+goto "hi" // Jump to a flag.
 print("Done") // Prints "Done" to Standard output
--- Runtimes/spearRuntime.lua
+++ Runtimes/spearRuntime.lua
@@ -1,9 +1,13 @@
 local config={
     TOC=100
 }
+local Args={...}
+local PCode=Args[1]
 local PC=1
-local LC=1
+local Line=""
 local vars={}
+vars.private={}
+vars.public={}
 local gotoFlags={}
 local nums={"1","2","3","4","5","6","7","8","9","0"}
 local chars={"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"}
@@ -21,7 +25,20 @@ local syntax={
             elseif type(val) == "number" then
                 PC = val
             else
-                error("invalid arg at line \""..tostring(PC).."\"")
+                error("error on line"..tostring(PC))
+            end
+        end,
+        set=function(...)
+            local args = {...}
+            local val = args[1]
+            if type(val) == "string" then
+                if isin(val,gotoFlags) then
+                    PC = gotoFlags[val]
+                end
+            elseif type(val) == "number" then
+                PC = val
+            else
+                error("error on line"..tostring(PC))
             end
         end,
         index=function(...)
@@ -30,7 +47,7 @@ local syntax={
             if type(val) == "string" then
                 return gotoFlags[val]
             else
-                error("invalid arg at line \""..tostring(PC).."\"")
+                error("error on line"..tostring(PC))
             end
         end
     },
@@ -46,18 +63,68 @@ local syntax={
     },
     ["private"]={
         set=function(...)
-            local args={...}
+            local args = {...}
             local varType = args[1]
             local nameSpace = args[2]
             local val = eval(args,4)
-            if canFit(val,varType) then
-                vars[nameSpace] = {
-                    ["type"]=varType,
-                    ["value"]=val
-                }
+            if not isin(nameSpace,syntax) then
+                if canFit(val,varType) then
+                    vars.private[nameSpace] = {
+                        ["type"]=varType,
+                        ["value"]=val
+                    }
+                    lookup
+                else
+                    error("error on line"..tostring(PC))
+                end
+            end
+        end
+    },
+    ["gflag"]={
+        set=function(...)
+            local args = {...}
+            local name = args[1]
+            if type(name) == "string" then
+                gotoFlags[name] = PC
             else
-                error("value out of range")
+                error("error on line"..tostring(PC))
             end
         end
     }
 }
+function isin(item,list,iorv)
+    for i,v in ipairs(list) do
+        if iorv then
+            if i == item then
+                return true
+            end
+        else
+            if v == item then
+                return true
+            end
+        end
+    end
+    return true
+end
+function getLines(filename)
+    local lines = {}
+    -- io.lines returns an iterator, so we need to manually unpack it into an array
+    for line in io.lines(filename) do
+        lines[#lines+1] = line
+    end
+    return lines
+end
+local PCode = getLines("test")
+while true do
+    Line = PCode[PC]
+    local i = 1
+    local subString = ""
+    while true do
+        if isin(subString,lookup) then
+            local command = subString
+            break
+        else
+            i = i + 1
+        end
+    end
+    
